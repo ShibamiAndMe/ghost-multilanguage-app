4d0116eb27be685997bd41f445a4801c806c60cb
kratos <davidlopez.david@gmail.com>
2018-04-07 18:17:03 +0200

    Added instagram helper

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const App = require('ghost-app');
+const request = require('sync-request');
 
 const MultiLang = App.extend({
 
@@ -8,12 +9,20 @@ const MultiLang = App.extend({
     uninstall: function () {},
 
     activate: function () {
-    	this.ghost.helpers.register('equal', this.equalHelper);
+		this.ghost.helpers.register('equal', this.equalHelper);
+		this.ghost.helpers.register('instagramWidget', this.instagramEmbed);
 	},
 
 	deactivate: function () {},
 
 	// Custom functions
+	/**
+	 * Compares lvalue with rvalue
+	 *
+	 * @param lvalue
+	 * @param rvalue
+	 * @param options
+	 */
 	equalHelper: function(lvalue, rvalue, options) {
 		if (arguments.length < 3)
 			throw new Error(`Handlebars Helper 'equal' needs 2 parameters. Actuals: ${lvalue} - ${rvalue}`);
@@ -22,6 +31,21 @@ const MultiLang = App.extend({
 		} else {
 			return options.inverse(this);
 		}
+	},
+
+	/**
+	 * Gets the embed code to insert into instagram widget
+	 *
+	 * @param user Instagram user
+	 */
+	instagramEmbed: function(user) {
+		const instagramURL = `https://www.instagram.com/${user}/?__a=1`;
+		const getInstagramJSON = request('GET', instagramURL);
+		const instagramData = JSON.parse(getInstagramJSON.getBody('utf-8'));
+		const mediaId = instagramData.graphql.user.edge_owner_to_timeline_media.edges[0].node.shortcode;
+		const url = `https://api.instagram.com/oembed/?url=http://instagr.am/p/${mediaId}`;
+		const res = request('GET', url);
+		return JSON.parse(res.getBody('utf8')).html;
 	}
 
 });
